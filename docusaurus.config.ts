934fa55bee5a5feb99ac476b3b3ab657d5997263
john.kim <johnzon3@gmail.com>
2025-03-19 23:49:26 +0900

    change config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://github.com/",
+  url: "https://touhouwiki-kr.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "touhou-kr", // Usually your GitHub org/user name.
   projectName: "touhouwiki-kr", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
